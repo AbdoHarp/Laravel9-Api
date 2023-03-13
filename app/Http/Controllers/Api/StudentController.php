13b77ae66d67864bce84c290c0d49d9412e00f83
abdo harp <abdoharp234@gmail.com>
2023-03-13 21:17:26 +0200

    first code with Rest Api

---
+++ app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $studend = Student::all();
+        if ($studend->count() > 0) {
+            return response()->json([
+                'status' => 200,
+                'student' => $studend,
+                200
+            ]);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No Records Found',
+                404
+            ]);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|max:191',
+            'phone' => 'required|digits:10',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
+        }
+        $studend = Student::create([
+            'name' => $request->name,
+            'course' => $request->course,
+            'email' => $request->email,
+            'phone' => $request->phone,
+        ]);
+        if ($studend) {
+            return response()->json([
+                'status' => 201,
+                'message' => 'Student Add Successfully',
+            ], 201);
+        }
+        return response()->json([
+            'status' => 500,
+            'message' => 'Something Wrong',
+        ], 500);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Api\StudentController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('students', [StudentController::class, 'index']);
+Route::post('students', [StudentController::class, 'store']);
+
