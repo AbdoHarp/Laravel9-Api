1081ae6715ad31f7c38b7900667676dad98e057c
abdo harp <abdoharp234@gmail.com>
2023-03-15 20:16:13 +0200

    Add comment up All Routes for student and Add routes for product Functions

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,11 +11,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// for student Routes 
 Route::get('students', [StudentController::class, 'index']);
 Route::post('students', [StudentController::class, 'store']);
 Route::get('students/{id}', [StudentController::class, 'show']);
 Route::get('students/{id}/edit', [StudentController::class, 'edit']);
 Route::put('students/{id}/edit', [StudentController::class, 'update']);
 Route::delete('students/{id}/delete', [StudentController::class, 'distroy']);
+
+
+// for  Product Routes
+Route::get('products', [ProductController::class, 'index']);
+Route::post('products', [ProductController::class, 'store']);
+Route::get('products/{id}', [ProductController::class, 'show']);
+Route::get('products/{id}/edit', [ProductController::class, 'edit']);
+Route::put('products/{id}/edit', [ProductController::class, 'update']);
+Route::delete('products/{id}/delete', [ProductController::class, 'distroy']);
 
