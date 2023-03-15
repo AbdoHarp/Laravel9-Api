2d478a73f2ed7ebee064d952a905b33e526f0357
abdo harp <abdoharp234@gmail.com>
2023-03-15 19:01:05 +0200

    Wirite code for edit,updated and delete function

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
@@ -56,7 +57,77 @@ class StudentController extends Controller
         }
         return response()->json([
             'status' => 500,
-            'message' => 'Something Wrong',
+            'message' => 'Something Wrong!',
         ], 500);
+    }
+
+    public function show($id)
+    {
+        $studend = Student::find($id);
+        if ($studend) {
+            return response()->json([
+                'status' => 200,
+                'message' => $studend,
+            ], 200);
+        }
+        return response()->json([
+            'status' => 404,
+            'message' => 'No such studnt Found!',
+        ], 404);
+    }
+
+    public function edit($id)
+    {
+        $studend = Student::find($id);
+        if ($studend) {
+            return response()->json([
+                'status' => 200,
+                'message' => $studend,
+            ], 200);
+        }
+        return response()->json([
+            'status' => 404,
+            'message' => 'No such student Found!',
+        ], 404);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|max:191',
+            'phone' => 'required|digits:11',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
+        }
+        $studend = Student::find($id);
+        if ($studend) {
+            $studend->update([
+                'name' => $request->name,
+                'course' => $request->course,
+                'email' => $request->email,
+                'phone' => $request->phone,
+            ]);
+            return response()->json([
+                'status' => 200,
+                'message' => 'Student Updated Successfully',
+            ], 200);
+        }
+        return response()->json([
+            'status' => 404,
+            'message' => 'No Such Student found!',
+        ], 404);
+    }
+
+    public function distroy(Request $request, $id)
+    {
+        $studend = Student::find($id);
+        
     }
 }
